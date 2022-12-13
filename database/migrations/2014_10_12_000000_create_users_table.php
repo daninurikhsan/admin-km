7a4c3fd4cd8dcb623cc8a6327060dc36241ff9da
daninurikhsan <priandani25@gmail.com>
2022-12-13 15:06:47 +0800

    add: authentication

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['dev','admin'])->default('admin');
+            $table->tinyInteger('is_active')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
